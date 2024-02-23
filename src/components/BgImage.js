8f906ffa6b0fe5b4fdced98963ed6bc89196c38c
truonguy <truonguy123tx@gmail.com>
2024-02-23 15:36:43 +0700

    first-weekk

--- src/components/BgImage.js
+++ src/components/BgImage.js
@@ -11,7 +11,7 @@ export default function BgImage() {
           height: "80vh",
         }}
       >
-        <div className="absolute inset-0 bg-[#F1900E] opacity-5"></div>
+        <div className=" inset-0 bg-[#F1900E] opacity-5"></div>
         <div className="bg-gray-900 bg-opacity-50 flex items-center h-full lg:pl-40 sm:pl-auto">
           <div className="px-4 lg:px-10 w-full lg:w-2/3">
             <h2 className="text-white w-4/6 text-3xl sm:text-5xl sm:leading-tight font-bold mb-8">
@@ -26,7 +26,7 @@ export default function BgImage() {
               outdoors and online. Enjoy the most flexible sports and wellness
               offer in Europe!
             </p>
-            <button className="flex items-center mt-4 px-6 py-4 bg-[#F1900E] hover:opacity-80 text-white text-sm uppercase focus:outline-none rounded-full font-open-sans">
+            <button className="flex items-center mt-4 px-6 py-4 bg-[#F1900E] hover:bg-opacity-80 text-white text-sm uppercase focus:outline-none rounded-full font-open-sans">
               <span className="font-bold">Discover Location</span>
             </button>
           </div>
